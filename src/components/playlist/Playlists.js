b85ea2610fadab4880d72c7e3c7e870c7c433808
Tukkos <jpfcastro@id.uff.br>
2023-02-03 15:10:00 -0300

    fix: playlists page when there is no playlist

--- src/components/playlist/Playlists.js
+++ src/components/playlist/Playlists.js
@@ -18,6 +18,7 @@ export default function Playlists() {
   const token = useToken();
   const userId = useUserId();
   const [ playlists, setPlaylists ] = useState([]);
+  console.log(playlists);
 
   useEffect(() => {
     getPlaylists({ token, userId })
@@ -29,49 +30,49 @@ export default function Playlists() {
       });
   }, []);
 
-  const userPlaylists = [
-    {
-      id: 0,
-      name: 'Beyonce',
-      image: 'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2022/07/beyonce-renaissance-foto.jpg',
-      duration: '00:30',
-      musics: [
-        {
-          id: 0,
-          name: 'Energy',
-          duration: 116000
-        },
-        {
-          id: 1,
-          name: 'Cozy',
-          duration: 210000
-        },
-        {
-          id: 2,
-          name: 'Break my Soul',
-          duration: 278000
-        }
-      ]
-    },
-    {
-      id: 1,
-      name: 'Billie Eilish',
-      image: 'https://upload.wikimedia.org/wikipedia/en/2/26/Billie_Eilish_-_Guitar_Songs.png',
-      duration: '00:08',
-      musics: [
-        {
-          id: 0,
-          name: 'TV',
-          duration: 282000
-        },
-        {
-          id: 1,
-          name: 'The 30th',
-          duration: 217000
-        }
-      ]
-    }
-  ];
+  // const userPlaylists = [
+  //   {
+  //     id: 0,
+  //     name: 'Beyonce',
+  //     image: 'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2022/07/beyonce-renaissance-foto.jpg',
+  //     duration: '00:30',
+  //     musics: [
+  //       {
+  //         id: 0,
+  //         name: 'Energy',
+  //         duration: 116000
+  //       },
+  //       {
+  //         id: 1,
+  //         name: 'Cozy',
+  //         duration: 210000
+  //       },
+  //       {
+  //         id: 2,
+  //         name: 'Break my Soul',
+  //         duration: 278000
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     id: 1,
+  //     name: 'Billie Eilish',
+  //     image: 'https://upload.wikimedia.org/wikipedia/en/2/26/Billie_Eilish_-_Guitar_Songs.png',
+  //     duration: '00:08',
+  //     musics: [
+  //       {
+  //         id: 0,
+  //         name: 'TV',
+  //         duration: 282000
+  //       },
+  //       {
+  //         id: 1,
+  //         name: 'The 30th',
+  //         duration: 217000
+  //       }
+  //     ]
+  //   }
+  // ];
 
   return (
     <DefaultScreen>
@@ -80,13 +81,12 @@ export default function Playlists() {
         <Link to="/generate"> <AiOutlinePlusCircle color='#F0F7F4' fontSize='30px' /> </Link >
         
       </Header>
-      {(userPlaylists[0] === undefined) ? 
+      {(playlists.length === 0) ? 
         <Body >
           <Message> No momento você ainda não tem playlists </Message >
         </Body>
         :
         <Body>
-          {userPlaylists.map((playlist) => <PlaylistComponent playlist={playlist} key={playlist.id} /> )}
           {playlists.map((playlist) => <PlaylistComponent playlist={playlist} key={playlist.id} /> )}
         </Body>
       }
